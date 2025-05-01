a16f8f2f93a04e5db193c50d5e231329d2f6d790
IDKArbiza <lucasarbiza15@gmail.com>
2025-05-01 15:45:37 -0300

    MSCV tipo_obligacion
    Antes no funcionaba bien, ahora si

--- app/Models/TipoObligacion.php
+++ app/Models/TipoObligacion.php
@@ -8,6 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class TipoObligacion extends Model
 {
     use HasFactory;
-
+    protected $table="tipo_obligaciones";
     protected $fillable = ['nombre_obligacion'];
 }
